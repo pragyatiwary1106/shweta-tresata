c4db5c0a7308f3b2b9c8a872b8170ba93618450d
pragyatiwary1106 <pragyatiwary1106@gmail.com>
2025-11-20 22:26:33 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -6,3 +6,37 @@ variable "key"{
 description = "key of storage account"
 type = string
 }
+variable "resource_group_name"{
+description = "resource group name"
+type = string
+}
+variable "location" {
+description = "location of rg"
+type = string
+}
+variable "aks_name"{
+description = "aks cluster name"
+type = string
+}
+variable "dns_prefix"{
+description = "prefix for dns"
+type = string
+}
+variable "node_name"{
+description = "node name"
+type = string
+}
+variable "node_count"{
+description = "node count"
+type = string
+}
+variable "vm_size"{
+description = "size of vm"
+type = string
+}
+variable "environment"{
+description = "prod/dev/staging"
+type = string
+}
+
+
